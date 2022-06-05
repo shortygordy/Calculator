3966d21c1041ebbc9473b296c9929ed9a8ae2b88
gordy <dimkagord@gmail.com>
2022-06-05 17:04:40 +0300

    implement final version of logic

--- main.js
+++ main.js
@@ -1,76 +1,95 @@
+//declare global variables
+let digit = 0;
+let currentNumber = '';
+let firstNumber = '';
+let currentOperator = '';
+let result = '';
+let secondNumber = '';
+let prevOperator = '';
+
 //OPERATE FUNCTIONS
-function add(num1, num2) {
-    return Number(num1) + Number(num2);
+function add(firstNumber, num2) {
+    return Number(firstNumber) + Number(num2);
 }
 
-function subtract(num1, num2) {
-    return Number(num1) - Number(num2);
+function subtract(firstNumber, num2) {
+    return Number(firstNumber) - Number(num2);
 }
 
-function multiply(num1, num2) {
-    return Number(num1) * Number(num2);
+function multiply(firstNumber, num2) {
+    return Number(firstNumber) * Number(num2);
 }
 
-function divide(num1, num2) {
-    return Number(num1) / Number(num2);
+function divide(firstNumber, num2) {
+    return Number(firstNumber) / Number(num2);
 }
 
-function operate(num1, num2, operator) {
+function operate(firstNumber, num2, operator) {
     switch (operator) {
         case '+':
-            return add(num1, num2);
+            return add(firstNumber, num2);
         case '−':
-            return subtract(num1, num2);
+            return subtract(firstNumber, num2);
         case '×':
-            return multiply(num1, num2);
+            return multiply(firstNumber, num2);
         case '÷':
-            return divide(num1, num2);
+            return divide(firstNumber, num2);
+        case '':
+            return;
     }
 }
 
-function filterDisplay(e) {
+function filterInput(e) {
+
+    display.style.fontSize = '30pt';
 
     //delete leading zero
-    if (display.textContent === '0' && (e.target.textContent !== '.' && e.target.classList[0] !== 'operator')) {
+    if (display.textContent === '0' && e.target.textContent !== '.' && !(e.target.classList[0] === 'operator' || e.target.classList[0] === 'equals')) {
         display.textContent = '';
-        currentNumber = '';
     }
 
     //disable showing operators in main display
-    if (e.target.classList[0] == 'operator') {
+    if (e.target.classList[0] === 'operator') {
         digit = '';
         return;
     }
 
+
+
     //disable having 2 decimals
-    if ((currentNumber.includes('.') && e.target.textContent === '.') || (currentOperator === '=' && e.target.textContent === '.' && num1 !== '')) {
+    if (e.target.textContent === '.' && String(currentNumber).includes('.') && (firstNumber === '' || secondNumber !== '')) {
         digit = '';
+        return;
+    }
+
+
+    if ((display.textContent === '0' && e.target.textContent === '.') || (firstNumber !== '' && secondNumber === '' && e.target.textContent === '.')) {
+        digit = '0.';
+        currentNumber = '';
         return;
     } else {
         digit = e.target.textContent;
     }
 }
 
+function updateDisplay(e) {
 
-function updateDisplay() {
+    //don't show operators or equals
+    if (e.target.classList[0] === 'operator' || e.target.classList[0] === 'equals') digit = '';
 
-    if (currentOperator !== '' && currentOperator !== '=' && secondNumber === '') {
-        display.textContent = '';
-    }
-    //add currentNumber to display
-    display.textContent += digit;
-    currentNumber += digit;
+    //set display to current number
+    display.textContent = currentNumber;
 }
 
 function clear() {
     digit = 0;
     currentNumber = '';
-    num1 = '';
+    firstNumber = '';
     currentOperator = '';
-    result = '';
     secondNumber = '';
     prevOperator = '';
     display.textContent = 0;
+    display.style.fontSize = '30pt';
 }
 
 function deleteNum() {
@@ -79,51 +98,86 @@ function deleteNum() {
     display.textContent = currentNumber;
 }
 
-function main(e) {
-    filterDisplay(e);
-    updateDisplay();
-
-    //if first number and second number are entered, and current input is an operator, solve
-    if (e.target.classList[0] === 'operator' && (num1 !== '' && secondNumber !== '')) {
-        result = operate(num1, secondNumber, currentOperator);
-
-        //display result and use it for next equation
-        display.textContent = Math.round(result * 1000) / 1000;
-        num1 = Number(result);
-        secondNumber = '';
-        currentNumber = '';
-        currentOperator = e.target.textContent;
-        return;
-    }
-
-    //if input is a first number, store it into num1
-    if (currentOperator === '' && e.target.classList[0] === 'button') {
-        num1 += digit;
-        return;
-    }
-
-    //if input is an operator, store it
-    if (e.target.classList[0] === 'operator') {
-
-        //if a saved operator doesn't exist, save it to the first one
-        if (currentOperator === '') {
-            currentOperator = e.target.textContent;
-            currentNumber = '';
-        } else {
-            //if it does exist, save the first one to prev, and update the current one
-            prevOperator = currentOperator;
-            currentOperator = e.target.textContent;
-        }
-        return;
-    }
-
-    //if input is a second number
-    if (num1 !== '' && e.target.classList[0] === 'button') {
-        secondNumber += digit;
-        return;
+function divideByZero() {
+    if ((secondNumber === '0' || secondNumber === '0.0') && prevOperator === '÷') {
+        return true;
     }
 }
 
+function main(e) {
+    filterInput(e);
+
+    //if equals is entered without having a second number, reset all numbers
+    if (e.target.textContent === '=' && secondNumber === '' && currentNumber === '') {
+        clear();
+        return;
+    }
+
+    //clear screen for second number
+    if (currentOperator !== '' && currentOperator !== '=' && firstNumber !== '' && secondNumber === '' && e.target.classList[0] !== 'operator' && e.target.classList[0] !== 'equals') {
+        console.log(e.target.classList[0]);
+        display.textContent = '';
+        currentNumber = '';
+    }
+
+    //if nothing has been entered, and a non-digit is pressed, do nothing
+    if (e.target.classList[0] !== 'digit' && currentNumber === '') return;
+
+    //if a number is pressed add it to current number
+    if (e.target.classList[0] === 'digit') {
+        currentNumber += digit;
+    }
+
+    //if there's no first number saved, and the current input is an operator, save to first number and save operator
+    if (firstNumber === '' && e.target.classList[0] === 'operator') {
+        firstNumber = currentNumber;
+        currentOperator = e.target.textContent;
+    }
+    //if the first number is saved, and the current input is an operator, save the operator
+    if (firstNumber !== '' && (e.target.classList[0] === 'operator' || e.target.classList[0] === 'equals')) {
+        prevOperator = currentOperator;
+        currentOperator = e.target.textContent;
+    }
+
+    //if the first number is saved, and the current input is a digit, save the current number to second number
+    if (firstNumber !== '' && e.target.classList[0] === 'digit') {
+        secondNumber += digit;
+    }
+
+    //if both numbers are saved, and the current input is an operator
+    if (secondNumber !== '' && e.target.classList[0] === 'operator') {
+        //solve
+        if (divideByZero() === true) {
+            clear();
+            display.style.fontSize = '13pt';
+            display.textContent = "Please don't divide by 0 ya filthy animal.";
+            return;
+        } else {
+            result = operate(firstNumber, secondNumber, prevOperator);
+            firstNumber = Math.round(result * 1000) / 1000;
+            secondNumber = '';
+            currentNumber = Math.round(result * 1000) / 1000;
+            display.textContent = Math.round(result * 1000) / 1000;
+            return;
+        }
+        //or current input is an equals
+    } else if (secondNumber !== '' && e.target.classList[0] === 'equals') {
+        if (divideByZero() === true) {
+            clear();
+            display.style.fontSize = '13pt';
+            display.textContent = "Please don't divide by 0 ya filthy animal.";
+            return;
+        } else {
+            result = operate(firstNumber, secondNumber, prevOperator);
+            clear();
+            firstNumber = Math.round(result * 1000) / 1000;
+            currentNumber = Math.round(result * 1000) / 1000;
+            display.textContent = Math.round(result * 1000) / 1000;
+            return;
+        }
+    }
+    updateDisplay(e);
+}
 
 //capture elements
 const inputButtons = document.querySelectorAll('#inputButtons div');
@@ -138,12 +192,3 @@ functionButtons.forEach(div => div.addEventListener('mouseup', (e) => div.classL
 inputButtons.forEach(div => div.addEventListener('click', (e) => main(e)));
 functionButtons[0].addEventListener('click', clear);
 functionButtons[1].addEventListener('click', deleteNum);
-
-//declare global variables
-let digit = 0;
-let currentNumber = '';
-let num1 = '';
-let currentOperator = '';
-let result = '';
-let secondNumber = '';
-let prevOperator = '';
